bc400fec6ecd13609f5a51aa628b623403172f45
RiccardoP <riccardo.pellico@gmail.com>
2015-10-08 12:14:18 +0200

    Revert "Varie"
    This reverts commit d666a2a5ab707a402f5d806c470711b556e72f52.

--- SCXML_datamodel/src/framework/Conf.java
+++ SCXML_datamodel/src/framework/Conf.java
@@ -9,7 +9,7 @@ public class Conf {
 	public static final String param_dir = "xml_conf";
 	public static final String model_name = "model";
 	public static final String scxml_extension = ".scxml";
-	public static final String xml_conf_extension="_conf.xml";
+	public static final String xml_Conf_extension="_conf.xml";
 	public static final String placeholder_for_code = "// here can be added code to specialize the behavior of this method";
 	public static final String warning_for_initial_state = "// but CANNOT be added any code interacting with the GUI - delete manually this comment when changing the initial state!";
 	public static final String endASMcurrentMethods = "// everything above this comment will be kept if this code is updated by ASMCodeCreator";
--- SCXML_datamodel/src/framework/GUICodeCreator.java
+++ SCXML_datamodel/src/framework/GUICodeCreator.java
@@ -1,33 +1,12 @@
 package framework;
 
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import org.jdom.Document;
-import org.jdom.Element;
-import org.jdom.JDOMException;
-import org.jdom.filter.ElementFilter;
-import org.jdom.filter.Filter;
-import org.jdom.input.SAXBuilder;
-import org.jdom.Element;
 
 public class GUICodeCreator {
 
-	private static Element Parameters;
-	
 	private GUICodeCreator() {
 		// with a private constructor instances of this class cannot be created,
 		// which is what we want since the actual generation of code is made
@@ -35,35 +14,11 @@ public class GUICodeCreator {
 	}
 
 	public static void create(String pSCName, List<String> variableNames, List<String> eventNames) throws IOException {
-		//LEGGO I PARAMETRI
-		Parameters=readConfFile(pSCName);
-		
-		//LEGGO IL DEFAULT
-		String[] Def = getDefaultButton();
-		
-		//SALVO I NOMI, RICORDARE DEFAULT
-		ArrayList<String> buttonNames= getEventNames();   //PROBLEMA: ATTRIBUTI MANCANTI E DEFAULT
-		//PRENDO I COLORI
-		ArrayList<String> colorsList= getcolors(Def[0]); //LI HO MESSI IN UPPERCASE
-		//PRENDO I TOOLTIPS
-		ArrayList<String> tooltipList=getTTList(Def[1]);
-		//PARAMETRI DI FRAME
-		String[] frameParam = new String[2];
-		frameParam[0]=Parameters.getChild("frame").getChild("width").getValue();
-		frameParam[1]=Parameters.getChild("frame").getChild("height").getValue();//QUESTI LI DEVO LEGGERE COME STRINGHE MA TANTO GLIELI DEVO SOLO FAR STAMPARE
-		
-
-		
-		
-		
-		
- 		BufferedWriter out = new BufferedWriter(new FileWriter(Conf.source_dir + Conf.filesep + pSCName + Conf.filesep + "ImplGUI.java"));
+		BufferedWriter out = new BufferedWriter(new FileWriter(Conf.source_dir + Conf.filesep + pSCName + Conf.filesep + "ImplGUI.java"));
 		String classContent = "";
 		classContent = writePreambleAndImports(pSCName);
-		classContent += Conf.linesep;
-		classContent += writeSignatureAndConstructor(frameParam, buttonNames, colorsList, tooltipList); //HO DOVUTO AGGIUNGERE I PARAMETRO PER EVITARE DI CREARE  ATTRIBUTI 
-																 //PER OGNI LISTA
-		classContent += writeButtons(); //QUI POSSO TOGLIERE IL PARAMETRO
+		classContent += writeSignatureAndConstructor();
+		classContent += writeButtons(eventNames);
 		classContent += writePanelsAndFields(variableNames);
 		classContent += writeTextArea();
 		classContent += writeGUIstartAndClassClosure();
@@ -80,7 +35,6 @@ public class GUICodeCreator {
 		result += "package " + pSCName + ";" + Conf.linesep + Conf.linesep;
 
 		// writing imports
-		result += "import java.util.HashMap;" + Conf.linesep; //MI SERVE QUESTO IMPORT PER LA HASH MAP
 		result += "import java.awt.Color;" + Conf.linesep;
 		result += "import java.awt.FlowLayout;" + Conf.linesep;
 		result += "import javax.swing.BoxLayout;" + Conf.linesep;
@@ -96,56 +50,37 @@ public class GUICodeCreator {
 		return result;
 	}
 
-		private static String writeSignatureAndConstructor(String[] frameParam, ArrayList<String> buttonNames, ArrayList<String> colorsList, ArrayList<String> tooltipList) {
+		private static String writeSignatureAndConstructor() {
 		String result = "";
 		// writing class signature
 		result += "public class ImplGUI extends AbstractGUI {" + Conf.linesep + Conf.linesep;
-		
-		//INSERISCO LA STAMPA DELLE LISTE TROVATE
-		result += writeButtonList(buttonNames);
-		result += writeColorList(colorsList);
-		result += writeTTList(tooltipList);
-		
-		
+
 		// writing constructor
 		result += "\tpublic ImplGUI(String pSCName) {" + Conf.linesep + Conf.linesep;
-
-		//INSERISCO LA HASH MAP DEL JFRAME
-		result += "\t\tHashMap<String,Integer> frameParameters = new HashMap<String,Integer>();" + Conf.linesep;
-		result += "\t\tString[] paramList = {" + "\"width\"" + ", " + "\"height\"" + "};" + Conf.linesep;
-		result += "\t\tInteger[] valueList = {" + frameParam[0] + ", " + frameParam[1] + "};" + Conf.linesep;
-		result += "\t\tfor (int i=0; i<paramList.length; i++) {" + Conf.linesep;
-		result += "\t\t\t frameParameters.put(paramList[i], valueList[i]);" + Conf.linesep;
-		result += "\t\t}" + Conf.linesep + Conf.linesep;
-		
 		result += "\t\tJFrame myFrame = new JFrame();" + Conf.linesep;
 		result += "\t\tmyFrame.setTitle(pSCName);" + Conf.linesep;
 		result += "\t\tmyFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);" + Conf.linesep;
-		result += "\t\tmyFrame.setSize(frameParameters.get(" + "\"width\"" + "), frameParameters.get(" + "\"height\"" + "));" + Conf.linesep;
+		result += "\t\tmyFrame.setSize(500, 800);" + Conf.linesep;
 		result += "\t\tmyFrame.setLayout(new FlowLayout());" + Conf.linesep + Conf.linesep;
 		return result;
 	}
 
-		private static String writeButtons() {
+		private static String writeButtons(List<String> eventNames) {
 		String result = "";
 		// writing buttons launching events labeling transitions
-//		result += "\t\tString[] eventList = { ";
-//		for (int j = 0; j < eventNames.size(); j++) {
-//			result += "\"" + eventNames.get(j) + "\"";
-//			if (j != eventNames.size() - 1)
-//				result += ", ";
-//		}
-		
-		
-//		result += " };" + Conf.linesep;
-		result += "\t\tfor (int i=0 ; i<eventList.length ; i++) {" + Conf.linesep;
-		result += "\t\t\tJButton aButton = new JButton(\"LAUNCH \" + eventList[i]);" + Conf.linesep;
-		result += "\t\t\taButton.setActionCommand(eventList[i]);" + Conf.linesep;
-		result += "\t\t\taButton.setBackground(eventColorValue[i]);" + Conf.linesep;
-		result += "\t\t\t aButton.setToolTipText(eventTTValue[i]);" + Conf.linesep;
+		result += "\t\tString[] eventList = { ";
+		for (int j = 0; j < eventNames.size(); j++) {
+			result += "\"" + eventNames.get(j) + "\"";
+			if (j != eventNames.size() - 1)
+				result += ", ";
+		}
+		result += " };" + Conf.linesep;
+		result += "\t\tfor (String event : eventList) {" + Conf.linesep;
+		result += "\t\t\tJButton aButton = new JButton(\"LAUNCH \" + event);" + Conf.linesep;
+		result += "\t\t\taButton.setActionCommand(event);" + Conf.linesep;
 		result += "\t\t\taButton.addActionListener(this);" + Conf.linesep;
 		result += "\t\t\tmyFrame.getContentPane().add(aButton);" + Conf.linesep;
-		result += "\t\t\teventButtons.put(eventList[i], aButton);" + Conf.linesep;
+		result += "\t\t\teventButtons.put(event, aButton);" + Conf.linesep;
 		result += "\t\t}" + Conf.linesep + Conf.linesep;
 		return result;
 	}
@@ -197,116 +132,4 @@ public class GUICodeCreator {
 		result += "\t}" + Conf.linesep;
 		return result;
 	}
-		
-		
-		//DA QUI IN GIU' HO CREATO IO
-		private static Element readConfFile(String file_name){
-			//Legge il file dei parametri e lo salva in Parameters una variabile della classe
-			File inputFile = new File(Conf.data_dir + Conf.filesep + Conf.param_dir + Conf.filesep + file_name + Conf.xml_conf_extension);
-			Element documentRoot = Common.getDocumentRoot(inputFile);
-			return documentRoot;	
-			}
-		
-		private static ArrayList<String> getEventNames(){
-			//Trova i nomi degli eventi, forse si può togliere dato che ce li ho in input
-			//ma così è più facile generalizzare credo
-			ArrayList<String> eventListNames= new ArrayList<String>();
-			Iterator<Element> anElement = Parameters.getDescendants(new ElementFilter("button"));
-			while (anElement.hasNext()) {
-				Element currentElement = anElement.next();
-				eventListNames.add(currentElement.getAttributeValue("id"));
-			}
-			return eventListNames;
-		}
-			
-		private static ArrayList<String> getcolors(String dcolor){
-			//Trova la lista dei colori completando col default quando mancanti
-			ArrayList<String> colorsList= new ArrayList<String>();
-			Iterator<Element> anElement = Parameters.getDescendants(new ElementFilter("color"));
-			while (anElement.hasNext()) {
-				Element currentElement = anElement.next();
-				if (!currentElement.getAttributeValue("expr").isEmpty()){
-					colorsList.add(currentElement.getAttributeValue("expr").toUpperCase());
-				}else{
-					colorsList.add(dcolor.toUpperCase());
-				}
-			}
-			return colorsList;
-		}
-		
-		private static ArrayList<String> getTTList(String dtooltip){
-			//Trova la lista dei tooltip completando col default quando mancanti
-			ArrayList<String> TTList= new ArrayList<String>();
-			Iterator<Element> anElement = Parameters.getDescendants(new ElementFilter("tooltip"));
-			while (anElement.hasNext()) {
-				Element currentElement = anElement.next();
-				if (!currentElement.getAttributeValue("expr").isEmpty()){
-					TTList.add(currentElement.getAttributeValue("expr"));
-				}else{
-					TTList.add(dtooltip);
-				}
-				
-			}
-			return TTList;
-		}
-		
-		private static String[] getDefaultButton(){
-			//Trova i parametri di default dei button
-			Element Default = Parameters.getChild("defaultb");
-			Iterator<Element> col =Default.getDescendants(new ElementFilter("dcolor") );
-			Element Col = col.next();
-			String dcolor=Col.getAttributeValue("expr");
-			
-			Iterator<Element> tol =Default.getDescendants(new ElementFilter("dtooltip") );
-			Element Tol = tol.next();
-			String dtooltip = Tol.getAttributeValue("expr");
-			String[] Ris = new String[2];
-			Ris[0]=dcolor;
-			Ris[1]=dtooltip;
-			return Ris;
-			
-		}
-		
-		private static String writeButtonList(List<String> eventNames) {
-			//Stampa sul file la lista dei bottoni
-			String result = "";
-			// writing the list of all buttons' names including the default 
-			result += "\tprivate String[] eventList = { ";
-			for (int j = 0; j < eventNames.size(); j++) {
-				result += "\"" + eventNames.get(j) + "\"";
-				if (j != eventNames.size() - 1)
-					result += ", ";
-			}
-			result += " };" + Conf.linesep;
-			return result;
-		}
-		
-		private static String writeColorList(List<String> colorNames) {
-			String result = "";
-			// writing the list of all colors names  
-			result += "\tprivate Color[] eventColorValue = { ";
-			for (int j = 0; j < colorNames.size(); j++) {
-				result +="Color." + colorNames.get(j);
-				if (j != colorNames.size() - 1)
-					result += ", ";
-			}
-			result += " };" + Conf.linesep;
-			return result;
-		}
-		
-		
-		private static String writeTTList(List<String> toolTips) {
-			String result = "";
-			// writing the list of all tooltips 
-			result += "\tprivate String[] eventTTValue = { ";
-			for (int j = 0; j < toolTips.size(); j++) {
-				result += "\"" + toolTips.get(j) + "\"";
-				if (j != toolTips.size() - 1)
-					result += ", ";
-			}
-			result += " };" + Conf.linesep;
-			return result;
-		}
-		
-		
 }
