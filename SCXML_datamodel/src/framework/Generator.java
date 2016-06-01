d1fc768e6128e9a700ee298ab354b2786e604230
asiole <asiole88@gmail.com>
2016-06-01 12:42:38 +0200

    prova

--- SCXML_datamodel/src/framework/Generator.java
+++ SCXML_datamodel/src/framework/Generator.java
@@ -92,6 +92,7 @@ public class Generator {
 	private static void compileSCfile(String pSCXMLModel, String pModelFileName) {
 		String cmd = "C:\\Program Files\\Java\\jdk1.8.0_60\\bin\\javac";
 		String cpSeparator = "";
+		String cpSeparato0r = "";
 		if (System.getProperty("os.name").startsWith("Mac OS") || System.getProperty("os.name").startsWith("Linux")) {
 			cpSeparator = ":";
 		} else
