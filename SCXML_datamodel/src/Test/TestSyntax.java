ae7a9001dbc47a1b7362160eb428093ba0273770
asiole <asiole88@gmail.com>
2016-07-18 19:04:41 +0200

    Aggiunti test

--- SCXML_datamodel/src/Test/TestSyntax.java
+++ SCXML_datamodel/src/Test/TestSyntax.java
@@ -13,7 +13,7 @@ public class TestSyntax {
 	public void testSCXMLDocumentSyntaxOK( ) {
 		SC_Model_Element SCXMLDocument = new SC_Model_Element();
 		SCXMLDocument.set_Model("contatore");
-		SCXMLDocument.set_documentRoot();
+		SCXMLDocument.set_documentRoot("");
 		assertTrue("SyntaxOk non funziona: il file è scritto correttamente",SCXMLDocument.SCXMLDocumentSyntaxOK());
 	}
 
@@ -21,7 +21,7 @@ public class TestSyntax {
 	public void testTarget( ) {
 		SC_Model_Element SCXMLDocument = new SC_Model_Element();
 		SCXMLDocument.set_Model("targeterrati");
-		SCXMLDocument.set_documentRoot2();
+		SCXMLDocument.set_documentRoot("SCxml_sbagliati");
 		assertTrue("SyntaxOk non funziona: qui i target sono errati", !SCXMLDocument.SCXMLDocumentSyntaxOK());
 	}
 
@@ -29,7 +29,7 @@ public class TestSyntax {
 	public void testData( ) {
 		SC_Model_Element SCXMLDocument = new SC_Model_Element();
 		SCXMLDocument.set_Model("dataerrati");
-		SCXMLDocument.set_documentRoot2();
+		SCXMLDocument.set_documentRoot("SCxml_sbagliati");
 		assertTrue("SyntaxOk non funziona: qui i data sono errati", !SCXMLDocument.SCXMLDocumentSyntaxOK());
 	}
 	
@@ -38,25 +38,82 @@ public class TestSyntax {
 	public void testVersion( ) {
 		SC_Model_Element SCXMLDocument = new SC_Model_Element();
 		SCXMLDocument.set_Model("versioneSbagliata");
-		SCXMLDocument.set_documentRoot2();
+		SCXMLDocument.set_documentRoot("SCxml_sbagliati");
 		assertTrue("SyntaxOk non funziona: qui la versione è errata", !SCXMLDocument.SCXMLDocumentSyntaxOK());
 	}
+	
 	
 
 	@Test
 	public void  TestEventCondTarget( ) {
 		SC_Model_Element SCXMLDocument = new SC_Model_Element();
 		SCXMLDocument.set_Model("eventcondtarget");
-		SCXMLDocument.set_documentRoot2();
+		SCXMLDocument.set_documentRoot("SCxml_sbagliati");
 		assertTrue("SyntaxOk non funziona: qui manca il target, l'evento e condizione a una transizione  ", !SCXMLDocument.SCXMLDocumentSyntaxOK());
 	}
 
+	@Test
+	public void  TestStatiStessoNome( ) {
+		SC_Model_Element SCXMLDocument = new SC_Model_Element();
+		SCXMLDocument.set_Model("statistessonome");
+		SCXMLDocument.set_documentRoot("SCxml_sbagliati");
+		assertTrue("SyntaxOk non funziona: esistono più stati con lo stesso nome  ", !SCXMLDocument.SCXMLDocumentSyntaxOK());
+	}
 	
+	@Test
+	public void  TestassignName( ) {
+		SC_Model_Element SCXMLDocument = new SC_Model_Element();
+		SCXMLDocument.set_Model("assignlocation");
+		SCXMLDocument.set_documentRoot("SCxml_sbagliati");
+		assertTrue("SyntaxOk non funziona: c'è assign non seguito da name  ", !SCXMLDocument.SCXMLDocumentSyntaxOK());
+	}
 	
+	@Test
+	public void  TestDataModel( ) {
+		SC_Model_Element SCXMLDocument = new SC_Model_Element();
+		SCXMLDocument.set_Model("datamodelinitial");
+		SCXMLDocument.set_documentRoot("SCxml_sbagliati");
+		assertTrue("SyntaxOk non funziona: ci sono 2 datamodel  ", !SCXMLDocument.SCXMLDocumentSyntaxOK());
+	}
 	
+	@Test
+	public void  TestXmlnsLink( ) {
+		SC_Model_Element SCXMLDocument = new SC_Model_Element();
+		SCXMLDocument.set_Model("xmlnsinitial");
+		SCXMLDocument.set_documentRoot("SCxml_sbagliati");
+		assertTrue("SyntaxOk non funziona: link xmlsbagliato ", !SCXMLDocument.SCXMLDocumentSyntaxOK());
+	}
 	
+	@Test
+	public void  TestInitialSbagliato( ) {
+		SC_Model_Element SCXMLDocument = new SC_Model_Element();
+		SCXMLDocument.set_Model("initialsbagliato");
+		SCXMLDocument.set_documentRoot("SCxml_sbagliati");
+		assertTrue("SyntaxOk non funziona: Initial è uno stato inesistente", !SCXMLDocument.SCXMLDocumentSyntaxOK());
+	}
 	
+	@Test
+	public void  TestInitialAssente( ) {
+		SC_Model_Element SCXMLDocument = new SC_Model_Element();
+		SCXMLDocument.set_Model("initialassente");
+		SCXMLDocument.set_documentRoot("SCxml_sbagliati");
+		assertTrue("SyntaxOk non funziona: Manca initial", !SCXMLDocument.SCXMLDocumentSyntaxOK());
+	}
 	
+	@Test
+	public void testDatadoppione( ) {
+		SC_Model_Element SCXMLDocument = new SC_Model_Element();
+		SCXMLDocument.set_Model("datadoppio");
+		SCXMLDocument.set_documentRoot("SCxml_sbagliati");
+		assertTrue("SyntaxOk non funziona: un data id è doppio", !SCXMLDocument.SCXMLDocumentSyntaxOK());
+	}
 	
+	/*@Test
+	public void testFinal( ) {
+		SC_Model_Element SCXMLDocument = new SC_Model_Element();
+		SCXMLDocument.set_Model("PROVA");
+		SCXMLDocument.set_documentRoot("SCxml_sbagliati");
+		assertTrue("SyntaxOk non funziona: un data id è doppio", !SCXMLDocument.SCXMLDocumentSyntaxOK());
+	}*/
 	
 }
